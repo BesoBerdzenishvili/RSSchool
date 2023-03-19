9d40e01680ae6dfad5c22a9783e09dcbb404e51d
fliderman01 <fliderman01@gmail.com>
2023-03-19 10:12:07 +0400

    feat: add header

---
+++ src/layout/Header.tsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Header.css';
+
+class Header extends React.Component {
+  state = {
+    currentPage: '/',
+  };
+
+  handleNavLinkClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    const currentPage = event.currentTarget.getAttribute('href');
+    this.setState({ currentPage });
+  };
+
+  render() {
+    const { currentPage } = this.state;
+
+    return (
+      <header>
+        <nav>
+          <ul className="menu">
+            <li>
+              <NavLink
+                to="/"
+                end
+                onClick={this.handleNavLinkClick}
+                className={({ isActive }) => (isActive ? 'active' : '')}
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/about"
+                onClick={this.handleNavLinkClick}
+                className={({ isActive }) => (isActive ? 'active' : '')}
+              >
+                About
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="current-page">
+          <span>Current page: </span>
+          {currentPage === '/'
+            ? 'Home'
+            : currentPage.slice(1)[0].toUpperCase() + currentPage.slice(2)}
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header;
