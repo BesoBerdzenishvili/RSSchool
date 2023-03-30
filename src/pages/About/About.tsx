b4f7a0c979c8bf07404ae5cc8fa10948cb94af2f
fliderman01 <fliderman01@gmail.com>
2023-03-30 12:59:52 +0400

    refactor: make About.tsx functional component

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,14 +1,10 @@
-import * as React from 'react';
-
-class About extends React.Component {
-  render() {
-    return (
-      <div>
-        <h2>About</h2>
-        <p>This is a project for RS School.</p>
-      </div>
-    );
-  }
-}
+const About = () => {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>This is a project for RS School.</p>
+    </div>
+  );
+};
 
 export default About;
