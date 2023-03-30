5bbe4d53faecdaccbf273637e0a1fb4237c75ea2
fliderman01 <fliderman01@gmail.com>
2023-03-30 11:58:10 +0400

    refactor: make Card.tsx functional component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { FormData } from '../../types/FormData';
 import './Card.css';
 
@@ -6,21 +5,19 @@ interface CardProps {
   data: FormData;
 }
 
-export class Card extends React.Component<CardProps> {
-  render() {
-    const { data } = this.props;
-    const { img, price, priceType, description, date } = data;
+const Card: React.FC<CardProps> = ({ data }) => {
+  const { img, price, priceType, description, date } = data;
 
-    return (
-      <div className="card">
-        {img && typeof URL !== 'undefined' && URL.createObjectURL && (
-          <img src={URL.createObjectURL(img)} alt="card image" width={300} />
-        )}
-        <div className="price">${price}</div> : <h4>POA</h4>
-        {<div className="guide-price">{priceType}</div>}
-        <div className="description">{description && description}</div>
-        {date && <div className="date">Added on: {date}</div>}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="card">
+      {img && typeof URL !== 'undefined' && URL.createObjectURL && (
+        <img src={URL.createObjectURL(img)} alt="card image" width={300} />
+      )}
+      <div className="price">${price}</div> : <h4>POA</h4>
+      {<div className="guide-price">{priceType}</div>}
+      <div className="description">{description && description}</div>
+      {date && <div className="date">Added on: {date}</div>}
+    </div>
+  );
+};
+export default Card;
