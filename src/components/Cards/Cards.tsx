c03ff1a6e8f9cb25f968c7e50d8bb762ce63c542
fliderman01 <fliderman01@gmail.com>
2023-03-30 12:27:57 +0400

    refactor: make Cards.tsx functional component

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
+import { useContext } from 'react';
 import { FormDataContext } from '../../contexts/formDataContext';
 import Card from '../Card/Card';
 import './Cards.css';
 
-export class Cards extends React.Component {
-  static contextType = FormDataContext;
-  declare context: React.ContextType<typeof FormDataContext>;
-  render() {
-    const { formData } = this.context;
+export const Cards = () => {
+  const { formData } = useContext(FormDataContext);
 
-    return (
-      <div className="cards">
-        {formData.length > 0 ? (
-          formData.map((i) => <Card key={i.id} data={i} />)
-        ) : (
-          <p className="add-message">
-            Please add data <a href="/add">here</a>
-          </p>
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="cards">
+      {formData.length > 0 ? (
+        formData.map((i) => <Card key={i.id} data={i} />)
+      ) : (
+        <p className="add-message">
+          Please add data <a href="/add">here</a>
+        </p>
+      )}
+    </div>
+  );
+};
--- src/contexts/formDataContext.tsx
+++ src/contexts/formDataContext.tsx
@@ -1,31 +1,22 @@
-import * as React from 'react';
+import React, { createContext, useState } from 'react';
 import { FormData } from '../types/FormData';
 
-export type FormDataContextType = {
+type FormDataContextType = {
   formData: FormData[];
-  setFormData: (data: FormData[]) => void;
+  setFormData: React.Dispatch<React.SetStateAction<FormData[]>>;
 };
 
-export const FormDataContext = React.createContext<FormDataContextType>({
+export const FormDataContext = createContext<FormDataContextType>({
   formData: [],
   setFormData: () => {},
 });
 
-interface FormDataProviderProps {
-  children: React.ReactNode;
-}
+export const FormDataProvider: React.FC<React.PropsWithChildren<{}>> = ({ children }) => {
+  const [formData, setFormData] = useState<FormData[]>([]);
 
-export class FormDataProvider extends React.Component<FormDataProviderProps, FormDataContextType> {
-  state: FormDataContextType = {
-    formData: [],
-    setFormData: (data: FormData[]) => {
-      this.setState({ formData: data });
-    },
-  };
-
-  render() {
-    return (
-      <FormDataContext.Provider value={this.state}>{this.props.children}</FormDataContext.Provider>
-    );
-  }
-}
+  return (
+    <FormDataContext.Provider value={{ formData, setFormData }}>
+      {children}
+    </FormDataContext.Provider>
+  );
+};
