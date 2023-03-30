16cafa44c664ff56b8a0471e5a602dbcc3d92585
fliderman01 <fliderman01@gmail.com>
2023-03-30 11:43:12 +0400

    reafctor: make Home.tsx functional component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,17 +1,14 @@
-import * as React from 'react';
 import { Cards } from '../../components/Cards/Cards';
 import Search from '../../components/Search/Search';
 import './Home.css';
 
-class Home extends React.Component {
-  render() {
-    return (
-      <div className="home">
-        <Search />
-        <Cards />
-      </div>
-    );
-  }
-}
+const Home = () => {
+  return (
+    <div className="home">
+      <Search value="" />
+      <Cards />
+    </div>
+  );
+};
 
 export default Home;
