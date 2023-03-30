70eea5834150e3e88d3d703b03893941838f6884
fliderman01 <fliderman01@gmail.com>
2023-03-30 11:44:09 +0400

    refactor: make NoMatch.tsx functional component

--- src/pages/NoMatch/NoMatch.tsx
+++ src/pages/NoMatch/NoMatch.tsx
@@ -1,10 +1,7 @@
-import * as React from 'react';
 import './NoMatch.css';
 
-class NoMatch extends React.Component {
-  render() {
-    return <h2 className="error-msg">404 Not Found</h2>;
-  }
-}
+const NoMatch = () => {
+  return <h2 className="error-msg">404 Not Found</h2>;
+};
 
 export default NoMatch;
