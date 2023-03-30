f98732c03ba751c077736d5e46e13361bdf6402c
fliderman01 <fliderman01@gmail.com>
2023-03-30 11:55:23 +0400

    refactor: make App.tsx functional component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FormDataProvider } from './contexts/formDataContext';
 import Home from './pages/Home/Home';
@@ -7,22 +6,20 @@ import NoMatch from './pages/NoMatch/NoMatch';
 import Header from './layout/Header/Header';
 import Add from './pages/Add/Add';
 
-class App extends React.Component {
-  render() {
-    return (
-      <FormDataProvider>
-        <BrowserRouter>
-          <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/add" element={<Add />} />
-            <Route path="*" element={<NoMatch />} />
-          </Routes>
-        </BrowserRouter>
-      </FormDataProvider>
-    );
-  }
-}
+const App = () => {
+  return (
+    <FormDataProvider>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/add" element={<Add />} />
+          <Route path="*" element={<NoMatch />} />
+        </Routes>
+      </BrowserRouter>
+    </FormDataProvider>
+  );
+};
 
 export default App;
