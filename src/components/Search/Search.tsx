64e11da40f77c1989917dc6b5ad63318b816e921
fliderman01 <fliderman01@gmail.com>
2023-04-05 11:50:00 +0400

    fix: Search save value on unmount restore on mount

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './Search.css';
 
-const Search = () => {
+const Search: React.FC = () => {
   const [searchValue, setSearchValue] = useState(() => {
     const saved = localStorage.getItem('searchValue');
     return saved ? JSON.parse(saved) : '';
@@ -11,11 +11,31 @@ const Search = () => {
     localStorage.setItem('searchValue', JSON.stringify(searchValue));
   }, [searchValue]);
 
+  useEffect(() => {
+    const handleBeforeUnload = () => {
+      localStorage.setItem('searchValue', JSON.stringify(searchValue));
+    };
+
+    window.addEventListener('beforeunload', handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+    };
+  }, [searchValue]);
+
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchValue(event.target.value);
   };
 
-  return <input type="text" className="search-input" onChange={handleChange} value={searchValue} />;
+  return (
+    <input
+      type="text"
+      className="search-input"
+      value={searchValue}
+      onChange={handleChange}
+      data-testid="search-input"
+    />
+  );
 };
 
 export default Search;
