ce83703c68d9684bdf01bd322710f532eeaad533
fliderman01 <fliderman01@gmail.com>
2023-04-07 18:58:47 +0400

    feat: add Modal

--- src/components/CharacterCard/CharacterCard.tsx
+++ src/components/CharacterCard/CharacterCard.tsx
@@ -1,21 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { CharacterCardData } from '../../types/DataTypes';
 import './CharacterCard.css';
+import CharacterModal from '../CharacterModal/CharacterModal';
 
 interface CharacterCardProps {
   info: CharacterCardData;
 }
 
 const CharacterCard: React.FC<CharacterCardProps> = ({ info }) => {
+  const [showModal, setShowModal] = useState(false);
   const { image, name, status } = info;
+
+  const handleClick = () => {
+    setShowModal(true);
+  };
   return (
-    <div className="character-card">
-      <img src={image} alt={name} className="character-img" />
-      <h2>Name: {name}</h2>
-      <p>
-        Status: <span style={{ color: status === 'Alive' ? 'green' : 'red' }}>{status}</span>
-      </p>
-    </div>
+    <>
+      <div className="character-card" onClick={handleClick}>
+        <img src={image} alt={name} className="character-img" />
+        <h2>Name: {name}</h2>
+        <p>
+          Status: <span style={{ color: status === 'Alive' ? 'green' : 'red' }}>{status}</span>
+        </p>
+      </div>
+      {showModal && <CharacterModal info={info} setShowModal={setShowModal} />}
+    </>
   );
 };
 
---
+++ src/components/CharacterModal/CharacterModal.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { CharacterCardData } from '../../types/DataTypes';
+import './CharacterModal.css';
+
+interface ModalProps {
+  info: CharacterCardData;
+  setShowModal: (show: boolean) => void;
+}
+
+const CharacterModal: React.FC<ModalProps> = ({ info, setShowModal }) => {
+  const { name, status, species, type, gender, origin, location, image, episode } = info;
+
+  const handleClick = (e: React.MouseEvent) => {
+    if (e.target === e.currentTarget) {
+      setShowModal(false);
+    }
+  };
+
+  return (
+    <div className="modal-overlay" onClick={handleClick}>
+      <div className="modal-container">
+        <button className="delete-btn" onClick={handleClick}>
+          X
+        </button>
+        <h2>Character Info</h2>
+        <img src={image} alt={name} className="modal-img" width="344px" />
+        <p>
+          <b>Name:</b> {name}
+        </p>
+        <p>
+          <b>Status:</b>{' '}
+          <span style={{ color: status === 'Alive' ? 'green' : 'red' }}>{status}</span>
+        </p>
+        <p>
+          <b>Species:</b> {species}
+        </p>
+        {type && (
+          <p>
+            <b>Type:</b> type
+          </p>
+        )}
+        <p>
+          <b>Gender:</b> {gender}
+        </p>
+        <p>
+          <b>Origin:</b> {origin.name}
+        </p>
+        <p>
+          <b>Location:</b> {location.name}
+        </p>
+        <p>
+          <b>Episodes:</b> {episode.length}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default CharacterModal;
