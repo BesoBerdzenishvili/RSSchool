e34914d1ac57c3adb528087e68662b667ab98713
fliderman01 <fliderman01@gmail.com>
2023-04-08 11:55:19 +0400

    feat: fetch character data

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,27 @@
+import { useState, useEffect } from 'react';
+import CharacterCards from '../../components/CharacterCards/CharacterCards';
 import Search from '../../components/Search/Search';
 import './Home.css';
+import Loading from '../../components/Loading/Loading';
 
 const Home = () => {
+  const [character, setCharacter] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch('https://rickandmortyapi.com/api/character?page=1')
+      .then((response) => response.json())
+      .then((data) => {
+        setCharacter(data.results);
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <div className="home">
       <Search />
+      {loading ? <Loading /> : <CharacterCards characters={character} />}
     </div>
   );
 };
