385516d7c95d0ad6b55a135b16f33118168f8468
fliderman01 <fliderman01@gmail.com>
2023-04-12 15:30:43 +0400

    refactor: vitest tests all files

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   test: {
+    globals: true,
     environment: 'jsdom',
+    setupFiles: './src/tests/setup.ts',
+    coverage: {
+      provider: 'c8',
+      all: true,
+      reporter: ['text'],
+    },
   },
 });
