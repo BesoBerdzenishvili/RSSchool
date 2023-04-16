d0109207855c07fa532e8db4d28a5b5b91428e92
fliderman01 <fliderman01@gmail.com>
2023-04-16 09:14:41 +0400

    refactor: display character data with redux

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,18 +1,16 @@
 import { useState, useEffect } from 'react';
+import { useGetSearchQuery } from '../../redux/characterApi';
 import CharacterCards from '../../components/CharacterCards/CharacterCards';
-import { CharacterCardData } from '../../types/DataTypes';
 import Search from '../../components/Search/Search';
 import Loading from '../../components/Loading/Loading';
 import './Home.css';
 
 const Home = () => {
-  const [characters, setCharacters] = useState<CharacterCardData[]>([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState('');
   const [searchValue, setSearchValue] = useState(() => {
     const saved = localStorage.getItem('searchValue');
     return saved ? saved : '';
   });
+  const { data, isLoading, error } = useGetSearchQuery(searchValue);
 
   useEffect(() => {
     localStorage.setItem('searchValue', searchValue);
@@ -30,34 +28,16 @@ const Home = () => {
     };
   }, [searchValue]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      setError('');
-      try {
-        const response = await fetch(
-          `https://rickandmortyapi.com/api/character/?name=${searchValue}&page=1`
-        );
-        const data = await response.json();
-        if (data.info) {
-          setCharacters(data.results);
-        } else if (data.error) {
-          setError(data.error);
-        }
-      } catch (error) {
-        console.log(error);
-        setError('Something went wrong...');
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchData();
-  }, [searchValue]);
-
   return (
     <div className="home">
       <Search setSearchValue={setSearchValue} />
-      {loading ? <Loading /> : error ? <p>{error}</p> : <CharacterCards characters={characters} />}
+      {isLoading ? (
+        <Loading />
+      ) : error ? (
+        <p>Something went wrong...</p>
+      ) : (
+        <CharacterCards characters={data.results} />
+      )}
     </div>
   );
 };
