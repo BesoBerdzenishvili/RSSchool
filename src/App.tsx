2f492f1c9f35019f45d3a09327e3726e6faf4693
fliderman01 <fliderman01@gmail.com>
2023-04-16 09:11:23 +0400

    refactor: delete formDataContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { FormDataProvider } from './contexts/formDataContext';
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import NoMatch from './pages/NoMatch/NoMatch';
@@ -8,17 +7,15 @@ import Form from './pages/Form/Form';
 
 const App = () => {
   return (
-    <FormDataProvider>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/form" element={<Form />} />
-          <Route path="*" element={<NoMatch />} />
-        </Routes>
-      </BrowserRouter>
-    </FormDataProvider>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/form" element={<Form />} />
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
